7aac63cc50169a486a387bc0b9d6ecf988037cc3
albinaa666 <albinaabdiganieva86@gmail.com>
2024-01-22 15:28:26 +0600

    dz2

---
+++ hw/app.js
@@ -0,0 +1,81 @@
+var arr = [34, 65, 67, 89,8,9,23,21,10,23,28]
+
+for(let i of arr){
+    if(i%2===0){
+        console.log(i)
+    }
+}
+
+var cinema = {
+    barboskiny:{
+        janr:"boyevik" ,
+        data_primyery:"1941" ,
+        prodoljitelnost : 1000000 ,
+        glavnye_oktery : "sobaki" ,
+        cost :"free",
+        dimension:"2 D",
+    },
+    mashak_i_medved:{
+        janr:"boyevik" ,
+        data_primyery:"1941" ,
+        prodoljitelnost : 20000 ,
+        glavnye_oktery : "medved" ,
+        cost :"free",
+        dimension:"3 D"
+    }
+}
+
+
+console.log(cinema.barboskiny)
+console.log(cinema.masha_i_medved)
+var x = Number(prompt(">"))
+
+switch (x) {
+    case 1:
+        alert( "I")
+    break;
+    case 2:
+        alert ("II")
+        break;
+    case 3:
+        alert("III")
+        break;
+    case 4:
+        alert("IV")
+        break;
+    case 5:
+        alert("V")
+        break;
+    case 6:
+        alert("VI")
+        break;
+    case 7:
+        alert("VII")
+        break;
+    case 8:
+        alert("VIII")
+        break;
+    case 9:
+        alert("IX")
+        break;
+    case 10:
+        alert("X")
+        break;
+    case 11:
+        alert("XI")
+        break;
+    case 12:
+        alert("XII")
+        break;
+    case 13:
+        alert("XIII")
+        break;
+    case 14:
+        alert("XIV")
+        break;
+    case 15:
+        alert("XV")
+        break;
+
+}
+
